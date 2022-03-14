6ab450cb3e5eaa875894e28341f06e194a78c375
chaimaa nmiyesse <chaimaanamisse@gmail.com>
2022-03-14 09:10:40 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { Admin, Resource } from 'react-admin';
+import restProvider from 'ra-data-simple-rest';
+import PostList from './components/PostList';
+
+function App() {
+  return (
+   <Admin dataProvider={restProvider('http://localhost:3000')}>
+     <Resource name='posts' list={PostList}></Resource>
+   </Admin>
+  );
+}
+
+export default App;
